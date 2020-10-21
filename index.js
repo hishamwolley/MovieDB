8c60ae5666522e30b43b13493ee647af989ed1d8
Hisham Wally <hisham.wolley@gmail.com>
2020-10-21 19:36:08 +0000

    Step3

--- index.js
+++ index.js
@@ -2,7 +2,27 @@ const express = require('express');
 
 const app = express();
 
-app.get('/',(req,res)=>{res.send('hi')})
+app.get('/',(req,res) => {
+    res.send('ok')
+})
+
+
+app.get('/test',(req,res) => {
+    res.send('{status:200, message:"ok"}')
+})
+
+
+app.get('/time',(req,res) => {
+
+    const hourDate = new Date();
+    const h = hourDate.getHours();
+    const m = hourDate.getMinutes();
+    const s = hourDate.getSeconds();
+    const time = `${h}:${m}:${s}`;
+
+    res.send(`{status:200, message:"${time}"}`)
+
+})
 
 const PORT = process.env.port || 5000;
 
