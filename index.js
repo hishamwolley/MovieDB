544b68a715120ca3a8f6cfaae7f9d95321f0bec4
Hisham Wally <hisham.wolley@gmail.com>
2020-10-21 18:59:25 +0000

    step 2

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+app.get('/',(req,res)=>{res.send('hi')})
+
 const PORT = process.env.port || 5000;
 
 app.listen(PORT, ()=>{console.log(`SERVER STARTED ${PORT}`)})
