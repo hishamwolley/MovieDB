2babc3cb70d8750cbe493661d2f4838966238bf4
Hisham Wally <hisham.wolley@gmail.com>
2020-10-22 19:11:30 +0000

    Step 6 & 7

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
+const { response } = require('express');
 const express = require('express');
 
 const app = express();
 
 const movies = [
-    { "title": "Joker", "year": 2019, "rating": 8.5 },
-    { "title": "Birdbox", "year": 1985, "rating": 6.6 },
-    { "title": "spy", "year": 2015, "rating": 7 },
-    { "title":"I Am Legend", "year": 2007, "rating": 7.2 }
+    { "title": "Jaws", "year": 1975, "rating": 8 },
+    { "title": "Avatar", "year": 2009, "rating": 7.8 },
+    { "title": "Brazil", "year": 1985, "rating": 8 },
+    { "title":"الإرهاب والكباب‎", "year": 1992, "rating": 6.2 }
 ]
 
 app.get('/movies/add',(req,res)=>{
@@ -18,22 +19,36 @@ app.get('/movies/get/',(req,res)=>{
 })
 
 app.get('/movies/get/by-date',(req,res)=>{
-    sortedMovies = movies.sort((a,b)=>a.year - b.year)
+    let sortedMovies = movies.sort((a,b)=>a.year - b.year)
     res.status(200).send(sortedMovies)
 })
 
 app.get('/movies/get/by-rating',(req,res)=>{
-    sortedMovies = movies.sort((a,b)=>a.rating - b.rating)
+    let sortedMovies = movies.sort((a,b)=>a.rating - b.rating)
     res.status(200).send(sortedMovies)
 })
 
 app.get('/movies/get/by-title',(req,res)=>{
-    sortedMovies = movies.sort((a,b) => (a.title > b.title) ? 1 : ((b.title > a.title) ? -1 : 0)); 
+    let sortedMovies = movies.sort((a,b) => (a.title > b.title) ? 1 : ((b.title > a.title) ? -1 : 0)); 
     res.status(200).send(sortedMovies)
 })
 
+
 app.get('/movies/edit',(req,res)=>{
     res.status(200).send('edit');
+})
+
+app.get('/movies/edit/id/:title',(req,res)=>{
+    
+    let movieTitle = movies.map(movie=>movie).filter(movie=>movie.title===req.params.title);
+
+    if(movieTitle){
+        if(movieTitle.length < 1){
+            res.status(404).send(`{status:404, error:true, message:'the movie ${req.params.title} does not exist'}`)
+        }else{
+            res.status(200).send(`{status:200, data: Movie: ${movieTitle[0].title}, Year: ${movieTitle[0].year}, Rating: ${movieTitle[0].rating}}`)
+        }
+    }
 })
 
 app.get('/movies/delete',(req,res)=>{
